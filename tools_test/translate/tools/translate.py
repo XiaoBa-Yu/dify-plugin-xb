c9ecb5fccfa10539afc5ee7b4ef8a2c8d9e20b8e
Xiaoba Yu <xb1823725853@gmail.com>
2025-02-11 15:53:31 +0800

    feat: markitdown

--- tools_test/translate/tools/translate.py
+++ tools_test/translate/tools/translate.py
@@ -1,34 +1,31 @@
 from collections.abc import Generator
 from typing import Any
-from googletrans import Translator
 from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
 
+from markitdown import MarkItDown
+from base64 import b64decode
+
 class TranslateTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
-        # 获取query参数
-        input_text = tool_parameters.get('query', '')
+        # 获取文件元数据
+        file_blob = tool_parameters.get('files')
         
-        try:
-            translator = Translator()
-            
-            # 检测输入文本的语言
-            detected = translator.detect(input_text)
-            
-            if detected.lang == 'en':
-                # 如果是英文，翻译成中文
-                result = translator.translate(input_text, dest='zh-cn').text
-            else:
-                # 如果不是英文，翻译成英文
-                result = translator.translate(input_text, dest='en').text
-            
-            # 返回三个固定变量：files、text、json
-            # 1. text 变量
-            yield self.create_text_message(result)
-    
-            
+        try:   
+            # base_url = tool_parameters.get("mode", "")
+            base_url = 'http://localhost:5001'
+            file_url = base_url + file_blob.url         
+            md = MarkItDown()
+            # 元数据只包含url 直接从URL转换
+            md_result = md.convert(file_url)
+               
+            yield self.create_text_message('markdown转换成功')
+            yield self.create_blob_message(
+                blob=md_result.text_content, meta={"mime_type": "text/markdown"}
+            )
+        
         except Exception as e:
-            error_msg = f"翻译错误: {str(e)}"
+            error_msg = f"转换错误: {str(e)}"
             # 返回错误信息
             yield self.create_text_message(error_msg)
             yield self.create_json_message({"error": error_msg})
