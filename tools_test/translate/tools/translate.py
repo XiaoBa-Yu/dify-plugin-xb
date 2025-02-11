604610a20133b32fd59090339d503b38532f6fb6
Xiaoba Yu <xb1823725853@gmail.com>
2025-02-11 16:13:53 +0800

    fix: bug

--- tools_test/translate/tools/translate.py
+++ tools_test/translate/tools/translate.py
@@ -4,7 +4,6 @@ from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
 
 from markitdown import MarkItDown
-from base64 import b64decode
 
 class TranslateTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
@@ -12,8 +11,7 @@ class TranslateTool(Tool):
         file_blob = tool_parameters.get('files')
         
         try:   
-            # base_url = tool_parameters.get("mode", "")
-            base_url = 'http://localhost:5001'
+            base_url = tool_parameters.get("mode")  or "http://localhost:5001"
             file_url = base_url + file_blob.url         
             md = MarkItDown()
             # 元数据只包含url 直接从URL转换
@@ -28,8 +26,3 @@ class TranslateTool(Tool):
             error_msg = f"转换错误: {str(e)}"
             # 返回错误信息
             yield self.create_text_message(error_msg)
-            yield self.create_json_message({"error": error_msg})
-            yield self.create_stream_variable_message(
-                variable_name="error",
-                variable_value=error_msg
-            )
